f221455a07ba20c2b8c7ec44f6beb70d569d37c8
29td <nonhlanhlamndebele9@gmail.com>
2022-06-21 10:26:39 +0200

    add JS to the top menu

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// let ham = document.querySelector(".ham");
+let ham = document.querySelector(".ham");
 let logo = document.querySelector(".logo");
 let icons = document.querySelector(".icons");
+let iconLink = document.querySelectorAll(".iconLink");
 
-   ham.addEventListener("click", ()=> {
+ham.addEventListener("click", () => {
         ham.classList.toggle("active");
         logo.style.visibility = "hidden";
-        icons.style.display = "none";
-   })
-
-   
+        icons.style.display = "block";
+    })
+    // iconLink.addEventListener("click", () => {
+    //     icons.style.display = "none";
+    // })
