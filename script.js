4884ffc1dede71b28574ecadb4d55e9bef963d3f
29td <nonhlanhlamndebele9@gmail.com>
2022-06-23 11:01:59 +0200

    add mobile popup

--- script.js
+++ script.js
@@ -1,23 +1,57 @@
-const openProjectButton = document.querySelector('#project');
-const closeProjectButtons = document.querySelector('#cross');
+let seeproject = document.getElementById('project');
+
+function openSeeproject() {
+  seeproject.classList.add('open-seeproject');
+  seeproject.style.visibility = 'hidden';
+  seeproject.style.display = 'block';
+}
+
+function closeSeeproject() {
+  seeproject.classList.add('open-seeproject');
+  seeproject.style.visibility = 'visible';
+  seeproject.style.display = 'none';
+}
+
+
+
+// const project = document.querySelectorAll('.seeProject');
+// const button = document.querySelectorAll('#ject');
+// var span = document.querySelector('.cross')[0];
+
+// button.addEventListener('click', () => {
+//   project.style.display = 'block';
+// }
+
+// ,span.addEventListener('click', () => {
+//   project.style.display = 'none';
+// }
+
+// ,window.addEventListener('click', () => {
+//   if (target == project) {
+//     project.style.display = 'none';
+//   }
+// }
+
+
+// const openProjectButton = document.querySelector('#ject');
+// const closeProjectButton = document.querySelector('#cross');
 // const lay = document.querySelector('#lay');
 
-openProjectButton.addEventListener('click', () => {
-  const work = document.querySelector('.work-content');
-  const div = document.createElement('div');
-  div.textContent = 'all';
+//   const work = document.querySelector('.work-content');
+//   const div = document.createElement('div');
+//   div.textContent = 'all';
 //   div.style.minHeight = '100vh';
 //   div.style.backgroundColor = 'red';
-  work.append(div);
-  work.classList.add('pop-up');
-})
+//   work.append(div);
+//   work.classList.add('pop-up');
+// })
 
 // body.appendChild(lay);
 
 
 // openProjectButtons.forEach(button => {
 //   button.addEventListener('click', () => {
-//     const project = document.querySelector(button.dataset.projectTarget);
+//     const project = document.querySelector(button.dataset.ject);
 //     openProject(project);
 //     })
 // })
