2e3a67d56b5d5d0f6559251bb2b866b2f88c8bbd
29td <nonhlanhlamndebele9@gmail.com>
2022-06-23 11:47:44 +0200

    add popup for desktop

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 let seeproject = document.getElementById('project');
-
+let card = `    <div class="seeproject" id="project">
+<div class="project-header">
+    <span class="cross">&times;</span>
+    <div class="project-title">Multi Post Stories</div>
+        
+</div>
+<div class="project-list">
+        <ul>
+            <li>html</li>
+            <li>bootstrp</li>
+            <li>Ruby on rails</li>
+        </ul>
+    </div>
+    <img src="./assets/images/Snap.png" alt="img" class="project-img"/>
+    <div class="project-body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In repellendus doloribus aliquid quas cum provident harum similique rerum illo nam non, totam vero earum vitae delectus, debitis incidunt voluptatibus ipsa!
+    </div>
+<div class="hub">
+    <button type="button" class="power">See Live<span><img src="./assets/icons/Icon.svg" alt="img"></button></span>
+    <button type="button" class="power">See Source<span><img src="./assets/icons/Vector.png" alt="img"></button></span>
+</div>
+</div>
+<div class="lay" id="lay"></div>
+`
 function openSeeproject() {
   seeproject.classList.add('open-seeproject');
   seeproject.style.visibility = 'hidden';
