580be1bf24f024e52893456feeae7beeba77c010
29td <nonhlanhlamndebele9@gmail.com>
2022-06-25 07:49:37 +0200

    change script

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+// hamburger
+const ham = document.querySelector('.ham');
+const logo = document.querySelector('.logo');
+const icons = document.querySelector('.icons');
+const iconLink = document.querySelector('.icon-link');
+const iconLink2 = document.querySelector('.icon-link2');
+const iconLink3 = document.querySelector('.icon-link3');
+
+ham.addEventListener('click', () => {
+  if (ham.classList.contains('active')) {
+    ham.classList.remove('active');
+    logo.style.visibility = 'visible';
+    icons.style.display = 'none';
+  } else {
+    ham.classList.add('active');
+    logo.style.visibility = 'hidden';
+    icons.style.display = 'block';
+  }
+});
+
+iconLink.addEventListener('click', () => {
+  ham.classList.toggle('active');
+  icons.style.display = 'none';
+  logo.style.visibility = 'visible';
+});
+
+iconLink2.addEventListener('click', () => {
+  ham.classList.toggle('active');
+  icons.style.display = 'none';
+  logo.style.visibility = 'visible';
+});
+
+iconLink3.addEventListener('click', () => {
+  ham.classList.toggle('active');
+  icons.style.display = 'none';
+  logo.style.visibility = 'visible';
+});
+
+// popup window
 const mobilePopup = document.querySelector('.menu-popup');
 const desktopPopup = document.querySelector('.desktop-popup');
 
