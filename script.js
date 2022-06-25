115cc937f21a2bf78f9def15df1f26224674ece6
29td <nonhlanhlamndebele9@gmail.com>
2022-06-25 20:58:23 +0200

    added a bug

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ const project1 = {
   name: 'Keeping track of hundreds  of components website',
   description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
   featuredImage: './assets/images/plc-dt.png',
-  technologies: ['html', 'bootstrap', 'Ruby on rails'],
+  technologies: [{'html': 'bootstrap', 'Ruby on rails':''},],
   linkToLiveVersion: 'https://29td.github.io/My-First-Portfolio/',
   linkToSource: 'https://github.com/29td/My-First-Portfolio',
 };
